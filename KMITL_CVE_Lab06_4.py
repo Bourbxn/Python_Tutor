9ad910c653ea46f3578369d816244718723d4a70
Bourbxn <jakkapat97@gmail.com>
2021-09-24 22:41:18 +0700

    MSI

--- KMITL_CVE_Lab06_4.py
+++ KMITL_CVE_Lab06_4.py
@@ -3,4 +3,4 @@ stringNum = input("Enter some numbers: ")
 lstNum = stringNum.split(' ')
 for i in range(len(lstNum)):
     lstNum[i]=int(lstNum[i])
-print(max(lstNum))
+print("Max value =",max(lstNum))

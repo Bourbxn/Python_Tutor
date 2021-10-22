40b27c3692ddd626e4dcdc72e24219248d1d50fd
Bourbxn <jakkapat97@gmail.com>
2021-10-22 23:13:46 +0700

    PC

---
+++ KMITL_CVE_Lab09_5.py
@@ -0,0 +1,35 @@
+print(" *** Median Mean ***")
+numList = input("Enter Number : ").split()
+sum = 0
+#Change to int list
+for i in range(len(numList)):
+    numList[i] = int(numList[i])
+print("list =",numList)
+#Find Mean
+mean = 0
+for i in range(len(numList)):
+    sum+=numList[i]
+if(sum%len(numList)==0):
+    mean = sum//len(numList)
+elif(sum%len(numList)!=0):
+    mean = sum/len(numList)
+print("Mean =",mean)
+#Sort List
+numList.sort()
+print("sort list =",numList)
+#Median
+median = 0
+posMid1 = 0
+posMid2 = 0
+if(len(numList)%2==0):
+    posMid1 = (len(numList)//2)-1
+    posMid2 = (len(numList)//2)
+    median = numList[posMid1]+numList[posMid2]
+    if(median%2==0):
+        median//=2
+    else:
+        median/=2
+else:
+    posMid1 = (len(numList)+1)//2
+    median = numList[posMid1]
+print("Median =",median)

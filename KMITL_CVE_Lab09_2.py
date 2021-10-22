52cb02222f396bc7f6a9e497ab4d792854a92d4e
Bourbxn <jakkapat97@gmail.com>
2021-10-22 22:37:34 +0700

    PC

--- KMITL_CVE_Lab09_2.py
+++ KMITL_CVE_Lab09_2.py
@@ -3,3 +3,5 @@ lst = input().split(" ")
 for i in range(len(lst)):
     lst[i]+=str(int(i+1))
 print(lst)
+for i in lst:
+    print(i,end=' ')
---
+++ KMITL_CVE_Lab09_3.py
@@ -0,0 +1,3 @@
+print(" *** Sequence Verification ***")
+numList = input().split(" ")
+for i in range

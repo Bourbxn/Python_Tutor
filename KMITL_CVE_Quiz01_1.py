36a83498f18d98d11f6f86977aa4237c0b18d848
Bourbxn <jakkapat97@gmail.com>
2021-10-26 14:21:34 +0700

    PC

--- KMITL_CVE_Quiz01_1.py
+++ KMITL_CVE_Quiz01_1.py
@@ -1,26 +1,28 @@
-def isValidWeightHeight(a,b):
-    if((a>0 and a<=200) or (b>0 and b<=300)):
+def isValidWeightHeight(a, b):
+    if (a > 0 and a <= 200) or (b > 0 and b <= 300):
         return True
     else:
         return False
 
-def bmiCalculator(a,b) :
-    bmi= a/((b/100)*(b/100))
-    if bmi<18.5:
+
+def bmiCalculator(a, b):
+    bmi = a / ((b / 100) * (b / 100))
+    if bmi < 18.5:
         print("underweight")
-    elif (bmi>=18.5 and bmi<=22.9):
-        print("normal")B
+    elif bmi >= 18.5 and bmi <= 22.9:
+        print("normal")
     else:
         print("overweight")
 
-a,b = input("Enter weight(kg) and height(cm): ").split()
+
+a, b = input("Enter weight(kg) and height(cm): ").split()
 try:
-    a,b = float(a),float(b)
+    a, b = float(a), float(b)
 except:
-    print('Not number')
+    print("Not number")
     quit()
-if isValidWeightHeight(a,b):
-    print('You are',end=' ')
-    bmiCalculator(a,b)
+if isValidWeightHeight(a, b):
+    print("You are", end=" ")
+    bmiCalculator(a, b)
 else:
-    print('Not valid weight or height.')
+    print("Not valid weight or height.")

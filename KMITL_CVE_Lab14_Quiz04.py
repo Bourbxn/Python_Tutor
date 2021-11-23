c22113b0f1ca1f28f6cda901986c16449c997e21
Bourbxn <jakkapat97@gmail.com>
2021-11-23 14:41:07 +0700

    PC

---
+++ KMITL_CVE_Lab14_Quiz04.py
@@ -0,0 +1,12 @@
+def diff(a, b):
+    x = int(b[0]) - int(a[0])
+    y = int(b[1]) - int(a[1])
+
+
+print("  Find difference between A(x1,y1) and B(x2,y2) ")
+ins = input("Enter x1 y1 x2 y2 : ").split()
+pointA = (int(ins[0]), int(ins[1]))
+pointB = (int(ins[2]), int(ins[3]))
+print("A B ==>", pointA, pointB)
+print("Differnce from", pointA, "to", pointB, "==>", diff(pointA, pointB))
+print("Differnce from", pointB, "to", pointA, "==>", diff(pointB, pointA))
--- KMITL_CVE_Lab14_Quiz05.py
+++ KMITL_CVE_Lab14_Quiz05.py
@@ -7,7 +7,7 @@ for i in range(len(text) // 2):
     dict[text[i * 2 + 1]] = int(text[i * 2])
 print(dict)
 # tuple
-tupl = list(dict.items())
+tupl = l4 = list(zip(dict.values(), dict.keys()))
 print(tupl)
 # tuple sort
 tupl.sort(reverse=True)
@@ -15,6 +15,6 @@ print(tupl)
 # Histrogram
 print("*** The Histogram of Age ***")
 for i in range(len(tupl)):
-    print(tupl[i][0][0:3], ":", end=" ")
-    print("=" * (int(tupl[i][1])), end="")
-    print(int(tupl[i][1]))
+    print(tupl[i][1][0:3], ":", end=" ")
+    print("=" * (int(tupl[i][0])), end="")
+    print(int(tupl[i][0]))

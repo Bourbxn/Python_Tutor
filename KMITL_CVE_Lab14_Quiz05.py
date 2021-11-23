b88f67c71f3e4f863e1ef2d013f0443d1bf23180
Bourbxn <jakkapat97@gmail.com>
2021-11-23 14:41:39 +0700

    PC

--- KMITL_CVE_Lab14_Quiz05.py
+++ KMITL_CVE_Lab14_Quiz05.py
@@ -7,7 +7,7 @@ for i in range(len(text) // 2):
     dict[text[i * 2 + 1]] = int(text[i * 2])
 print(dict)
 # tuple
-tupl = l4 = list(zip(dict.values(), dict.keys()))
+tupl = list(zip(dict.values(), dict.keys()))
 print(tupl)
 # tuple sort
 tupl.sort(reverse=True)

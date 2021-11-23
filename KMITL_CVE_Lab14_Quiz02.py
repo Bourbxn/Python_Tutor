f2beba6a1055b91f14eaf2fb9ea0f3257c19a2de
Bourbxn <jakkapat97@gmail.com>
2021-11-23 14:12:04 +0700

    PC

--- KMITL_CVE_Lab14_Quiz02.py
+++ KMITL_CVE_Lab14_Quiz02.py
@@ -2,7 +2,10 @@ text = input("Enter : ").split(" ")
 dict_old = {"Volkswagan": 1, "Toyota": 2, "Tesla": 2}
 print("dict_old =", dict_old)
 for i in range(len(text) // 2):
-    print(text[i * 2], "exists in dicts")
+    if text[i * 2] in dict_old:
+        print(text[i * 2], "exists in dicts")
+    else:
+        print(text[i * 2], "does not exist")
 # Create new dict
 for i in range(len(text) // 2):
     if text[i * 2] in dict_old:
--- KMITL_CVE_Lab14_Quiz05.py
+++ KMITL_CVE_Lab14_Quiz05.py
@@ -6,5 +6,9 @@ dict = {}
 for i in range(len(text) // 2):
     dict[text[i * 2 + 1]] = int(text[i * 2])
 print(dict)
-#tuple
-tupl = list
+# tuple
+tupl = list(dict.items())
+print(tupl)
+# tuple sort
+tupl.sort(reverse=True)
+print(tupl)
